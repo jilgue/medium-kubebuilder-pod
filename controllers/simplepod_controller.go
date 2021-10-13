ae06d65be4465807fed9515849d1f45b40f8d94b
César M. Cristóbal <cesar@callepuzzle.com>
2021-10-13 18:36:55 +0200

    simple pod

--- controllers/simplepod_controller.go
+++ controllers/simplepod_controller.go
@@ -18,7 +18,10 @@ package controllers
 
 import (
 	"context"
+	"strings"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -36,6 +39,7 @@ type SimplePodReconciler struct {
 //+kubebuilder:rbac:groups=medium.callepuzzle.com,resources=simplepods,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=medium.callepuzzle.com,resources=simplepods/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=medium.callepuzzle.com,resources=simplepods/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,16 +51,62 @@ type SimplePodReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *SimplePodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// your logic here
+	var instance mediumv1alpha1.SimplePod
+	errGet := r.Get(ctx, req.NamespacedName, &instance)
+	if errGet != nil {
+		log.Error(errGet, "Error getting instance")
+		return ctrl.Result{}, client.IgnoreNotFound(errGet)
+	}
+
+	pod := NewPod(&instance)
+
+	_, errCreate := ctrl.CreateOrUpdate(ctx, r.Client, pod, func() error {
+		return ctrl.SetControllerReference(&instance, pod, r.Scheme)
+	})
+
+	if errCreate != nil {
+		log.Error(errCreate, "Error creating pod")
+		return ctrl.Result{}, nil
+	}
+
+	err := r.Status().Update(context.TODO(), &instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func NewPod(pod *mediumv1alpha1.SimplePod) *corev1.Pod {
+	labels := map[string]string{
+		"app": pod.Name,
+	}
+
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      pod.Name,
+			Namespace: pod.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    "busybox",
+					Image:   "busybox",
+					Command: strings.Split(pod.Spec.Command, " "),
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyOnFailure,
+		},
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *SimplePodReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&mediumv1alpha1.SimplePod{}).
+		Owns(&corev1.Pod{}).
 		Complete(r)
 }
